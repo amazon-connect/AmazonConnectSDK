1d2e89b328be8abcfd33201475f07bd695674366
Bomi Lee <bomilee@amazon.com>
2023-09-28 14:46:13 -0400

    remove amzn jest reporter

--- core/jest.config.js
+++ core/jest.config.js
@@ -1,7 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-
 module.exports = {
   testEnvironment: "node",
-  coverageDirectory: "build/brazil-documentation/coverage",
-  reporters: ["default", ["@amzn/jest-reporter", { language: "typescript" }]],
+  reporters: ["default"],
 };
