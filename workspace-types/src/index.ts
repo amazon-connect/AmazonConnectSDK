aa9aa3e1c9aed74eef2f6a0350af156fee7b88ac
Craig Beall <beallcr@amazon.com>
2023-11-02 09:23:01 -0400

    Updated workspace types to support message origin

--- workspace-types/src/index.ts
+++ workspace-types/src/index.ts
@@ -1,3 +1,4 @@
 export type { AppConfig, IFrameAppConfig } from "./app-config";
 export type * from "./app-message";
 export type { LifecycleStage } from "./lifecycle-stage";
+export type { AppMessageOrigin } from "./message-origin";
---
+++ workspace-types/src/message-origin.ts
@@ -0,0 +1,5 @@
+export type AppMessageOrigin = {
+  _type: "app";
+  origin: string;
+  path: string;
+};
