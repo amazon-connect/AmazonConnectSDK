51c3fe67f43d3c8b8653c14cb764ac924f48df69
amazonconnectsdk <connect-sdk@amazon.com>
2023-11-03 16:45:10 -0400

    Releasing v1.0.1-rc.1

--- theme/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  testEnvironment: "jsdom",
-  coverageDirectory: "build/brazil-documentation/coverage",
-  reporters: ["default", ["@amzn/jest-reporter", { language: "typescript" }]],
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/'
-  ]
-};
