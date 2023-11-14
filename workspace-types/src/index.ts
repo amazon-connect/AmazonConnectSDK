c23bb9bbd0487ef3c601f87221c0961ef5ff63c4
Kadir Samiappan <kadirspn@amazon.com>
2023-11-14 15:02:09 -0500

    Merge pre-ga to mainline and replace subscriptions & publications with permissions

--- workspace-types/src/index.ts
+++ workspace-types/src/index.ts
@@ -1,5 +1,5 @@
 export type { AppConfig, IFrameAppConfig } from "./app-config";
 export type * from "./app-message";
-export type { AppScope, ContactScope } from "./app-scope";
+export type { AppScope, ContactScope, ZeroScope } from "./app-scope";
 export type { LifecycleStage } from "./lifecycle-stage";
 export type { AppMessageOrigin } from "./message-origin";
