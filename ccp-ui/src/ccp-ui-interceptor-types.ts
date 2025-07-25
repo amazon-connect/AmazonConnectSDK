32125dc396ec967d31d40261c0c7f2ec35aaa26f
amazonconnectsdk <connect-sdk@amazon.com>
2025-07-25 14:17:52 -0400

    Releasing v1.0.7-rc.13

--- ccp-ui/src/ccp-ui-interceptor-types.ts
+++
@@ -1,7 +0,0 @@
-import { Interceptor } from "./ui-extensibility";
-
-export type QuickConnectOpenContext = { contactId: string | null };
-export type QuickConnectOpenInterceptor = Interceptor<QuickConnectOpenContext>;
-export type OutboundDialerOpenContext = { contactId: string | null };
-export type OutboundDialerOpenInterceptor =
-  Interceptor<OutboundDialerOpenContext>;
